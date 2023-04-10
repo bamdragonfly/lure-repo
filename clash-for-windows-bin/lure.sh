7900551e17d8b6e56241e2e6cb1c0a54e25c912f
bamdragonfly <58493435+bamdragonfly@users.noreply.github.com>
2023-04-11 00:32:14 +0800

    update cfw lure.sh

--- clash-for-windows-bin/lure.sh
+++ clash-for-windows-bin/lure.sh
@@ -8,19 +8,24 @@ license=('Unlicense')
 maintainer='bamcat <bamdragonfly@gmail.com>'
 provides=('clash-for-windows-bin')
 conflicts=('clash-for-windows')
-sources_amd64=("https://github.com/Fndroid/clash_for_windows_pkg/releases/download/$version/Clash.for.Windows-$version-x64-linux.tar.gz")
-checksums_amd64=('SKIP')
+sources=(
+	"git+https://github.com/bamdragonfly/clash_for_windows_pkg.git"
+	"https://github.com/Fndroid/clash_for_windows_pkg/releases/download/$version/Clash.for.Windows-$version-x64-linux.tar.gz"
+	)
+checksums=(
+	'SKIP'
+	'SKIP'
+          )
 deps_fedora=('libXss.so.1' 'gtk3' 'nftables' 'iproute')
 deps_opensuse=('libXss1' 'gtk3' 'nftables' 'iproute2')
 
 package() {
-    local parch=$(echo ${CARCH} | sed "s/x86_64/x64/;s/aarch64/arm64/")
-    cd "Clash for Windows-${pkgver}-${parch}-linux"
+    cd "Clash for Windows-$version-x64-linux"
     echo "Packaging application files. Please wait."
-    install -d "${pkgdir}/opt/${pkgname}"
-    cp -r ./* "${pkgdir}/opt/${pkgname}"
-    install -Dm 755 ../cfw ${pkgdir}/usr/bin/cfw
-    install -Dm 644 ../clash.png ${pkgdir}/usr/share/pixmaps/clash.png
-    install -Dm 644 ../clash-for-windows.desktop ${pkgdir}/usr/share/applications/clash-for-windows.desktop
-    sed -e "s/VERSION_PLACEHOLDER/${pkgver}/g" -i ${pkgdir}/usr/share/applications/clash-for-windows.desktop
+    install -d "$pkgdir/opt/$name"
+    cp -r ./* "$pkgdir/opt/$name"
+    install -Dm 755 $srcdir/clash_for_windows_pkg/cfw $pkgdir/usr/bin/cfw
+    install -Dm 644 $srcdir/clash_for_windows_pkg/clash.png $pkgdir/usr/share/pixmaps/clash.png
+    install -Dm 644 $srcdir/clash_for_windows_pkg/clash-for-windows.desktop $pkgdir/usr/share/applications/clash-for-windows.desktop
+    sed -e "s/VERSION_PLACEHOLDER/$pkgver/g" -i $pkgdir/usr/share/applications/clash-for-windows.desktop
 }
