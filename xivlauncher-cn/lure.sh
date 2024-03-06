5529926bf49dfcd8aede02b882f70a4bbf2bea4f
bamdragonfly <58493435+bamdragonfly@users.noreply.github.com>
2024-03-06 10:02:36 +0800

    Update lure.sh

--- xivlauncher-cn/lure.sh
+++ xivlauncher-cn/lure.sh
@@ -16,7 +16,7 @@ build_deps_opensuse=('dotnet-sdk-6.0' 'aria2' 'libSDL2-2_0-0' 'libsecret-1-0' 'l
 
 build_deps_arch=('dotnet-sdk-6.0' 'python-yaml' 'git' 'aria2' 'sdl2' 'lib32-sdl2' 'libsecret' 'attr' 'lib32-attr' 'fontconfig' 'lib32-fontconfig' 'lcms2' 'lib32-lcms2' 'libxml2' 'lib32-libxml2' 'libxcursor' 'lib32-libxcursor' 'libxrandr' 'lib32-libxrandr' 'libxdamage' 'lib32-libxdamage' 'libxi' 'lib32-libxi' 'gettext' 'lib32-gettext' 'freetype2' 'lib32-freetype2' 'glu' 'lib32-glu' 'libsm' 'lib32-libsm' 'gcc-libs' 'lib32-gcc-libs' 'libpcap' 'lib32-libpcap' 'faudio' 'lib32-faudio' 'desktop-file-utils' 'jxrlib')
 
-sources=("git+https://github.com/ottercorp/XIVLauncher.Core.git#commit=dfa3f5a2ddf22dee624c8220badb79893b2bc4d3")
+sources=("git+https://github.com/ottercorp/XIVLauncher.Core.git#commit=225672268af4b0944b367d573df5f46d9ba7fc17")
 checksums=('SKIP')
 
 prepare() {
