21426d87c94c20ddab75c32a0510f342068be261
bamdragonfly <58493435+bamdragonfly@users.noreply.github.com>
2024-04-10 20:38:28 +0800

    Update lure.sh

--- xivlauncher-cn-git/lure.sh
+++ xivlauncher-cn-git/lure.sh
@@ -16,7 +16,7 @@ build_deps_opensuse=('git' 'aria2' 'libSDL2-2_0-0' 'libsecret-1-0' 'libattr1' 'f
 
 build_deps_arch=('python-yaml' 'git' 'aria2' 'sdl2' 'lib32-sdl2' 'libsecret' 'attr' 'lib32-attr' 'fontconfig' 'lib32-fontconfig' 'lcms2' 'lib32-lcms2' 'libxml2' 'lib32-libxml2' 'libxcursor' 'lib32-libxcursor' 'libxrandr' 'lib32-libxrandr' 'libxdamage' 'lib32-libxdamage' 'libxi' 'lib32-libxi' 'gettext' 'lib32-gettext' 'freetype2' 'lib32-freetype2' 'glu' 'lib32-glu' 'libsm' 'lib32-libsm' 'gcc-libs' 'lib32-gcc-libs' 'libpcap' 'lib32-libpcap' 'faudio' 'lib32-faudio' 'desktop-file-utils' 'jxrlib')
 
-sources=("git+https://github.com/ottercorp/XIVLauncher.Core.git#commit=d3c99fd50153af48a9462fb8807af462dd191155"
+sources=("git+https://github.com/ottercorp/XIVLauncher.Core.git"
          "https://download.visualstudio.microsoft.com/download/pr/19144d78-6f95-4810-a9f6-3bf86035a244/23f4654fc5352e049b517937f94be839/dotnet-sdk-6.0.421-linux-x64.tar.gz?~archive=false")
 checksums=(
             'SKIP'
